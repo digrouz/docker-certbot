fdb40ae5d734cf5ff9e57e812cf639c1582a9b04
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2016-10-06 10:13:47 +0000

    added docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ "$1" = 'certbot' ]; then
+   if [ -z "${DOCKMAIL}" ]; then
+      /usr/bin/logger "ERROR: administrator email is mandatory"
+   elif [ -z "${DOCKDOMAINS}" ]; then
+      /usr/bin/logger "ERROR: at least one domain must be specified"
+   else
+      exec /usr/bin/certbot certonly --verbose --noninteractive --quiet --standalone --agree-tos --email="${DOCKMAIL}" -d "${DOCKDOMAINS}" 
+   fi
+elif [ "$1" = 'certbot-renew' ]; then
+   exec /usr/bin/certbot renew
+fi
+
+exec "$@"
